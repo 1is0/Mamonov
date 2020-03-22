9537822a4ab18dc79a2af9b98a492b93bd6442e4
Andy258852 <62135208+Andy258852@users.noreply.github.com>
2020-03-22 20:22:53 +0200

    Add files via upload

---
+++ ex6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder s = new StringBuilder(Console.ReadLine());
+            Random num = new Random();
+            for(int i = 0; i < s.Length; i++)
+            {
+                int index = num.Next(0, s.Length);
+                char vo_vrema = s[index];
+                s[index] = s[i];
+                s[i] = vo_vrema;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
