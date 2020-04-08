d09828fdcdd40fb8d7088c46c79e6351361391fa
Andy258852 <62135208+Andy258852@users.noreply.github.com>
2020-04-08 22:57:41 +0300

    Create Economist.cs

---
+++ lab3/Economist.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace CSlab3
+{
+    class Economist
+    {
+        public Student Data { get; set; }
+        public int Mark { get; set; }
+
+        public Economist() { }
+        public Economist(University Location, int Year, int Age, string Name, int mark)
+        {
+            this.Mark = mark;
+            Data = new Student(Location, Year, Age, Name);
+        }
+        public Economist(Student student, int mark)
+        {
+            Data = student.GetDeepCopy();
+            this.Mark = mark;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString() + ", mark = " + Mark.ToString();
+        }
+
+        public static void SwapMarks(Economist a, Economist b)
+        {
+            int temp = a.Mark;
+            a.Mark = b.Mark;
+            b.Mark = temp;
+        }
+
+        public void ToCry()
+        {
+            Console.WriteLine("You are crying!!!");
+        }
+    }
+}
