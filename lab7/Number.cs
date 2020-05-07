4227bb1f5878887c5fb600fde87a72d01d3fd240
Andy258852 <62135208+Andy258852@users.noreply.github.com>
2020-05-07 18:38:38 +0300

    Add files via upload

---
+++ lab7/Number.cs
@@ -0,0 +1,235 @@
+﻿using System;
+
+namespace CSLab7
+{
+    class Number : IEquatable<Number>
+    {
+        private int N { get; set; }
+        private int M { get; set; }
+
+        public Number(int N, int M)
+        {
+            if (M <= 0)
+            {
+                throw new Exception("M is natural number");
+            }
+            ToCut(ref N, ref M);
+            SetValues(N, M);          
+        }
+        public Number(Number number)
+        {
+            SetValues(N, M);
+        }
+        public Number(float number)
+        {
+            SetValues(((Number)number).N, M = ((Number)number).M);
+        }
+        public Number(double number)
+        {
+            SetValues(((Number)number).N, M = ((Number)number).M);
+        }
+        public Number(int number)
+        {
+            SetValues(((Number)number).N, M = ((Number)number).M);
+        }
+
+        public static implicit operator double(Number number)
+        {
+            return Convert.ToDouble(number.ToSingle());
+        }
+        public static implicit operator float(Number number)
+        {
+            return number.ToSingle();
+        }
+        public static implicit operator int(Number number)
+        {
+            return Convert.ToInt32(Math.Round((float)number));
+        }
+        public static implicit operator string(Number number)
+        {
+            return number.ToString();
+        }
+        public static implicit operator Number(float number)
+        {
+            int b = 1;
+            while (number != Math.Truncate(number))
+            {
+                b *= 10;
+                number *= 10;
+            }
+            int N = Convert.ToInt32(number);
+            int M = b;
+            ToCut(ref N, ref M);
+            return new Number(N, M);
+        }
+        public static implicit operator Number(double number)
+        {
+            return new Number(Convert.ToSingle(number));
+        }
+        public static implicit operator Number(int number)
+        {
+            return new Number(Convert.ToSingle(number));
+        }
+        public static implicit operator Number(string number)
+        {
+            try
+            {
+                return new Number(Convert.ToSingle(number));
+            }
+            catch
+            {
+                throw new Exception("NaN");
+            }
+        }
+
+        public static Number operator +(Number a, Number b)
+        {
+            long ar1 = Convert.ToInt64(a.N);
+            long br1 = Convert.ToInt64(a.M);
+            long ar2 = Convert.ToInt64(b.N);
+            long br2 = Convert.ToInt64(b.M);
+            ar1 = ar1 * br2 + br1 * ar2;
+            br1 = br1 * br2;
+            ToCut(ref ar1, ref br1);
+            return new Number(Convert.ToInt32(ar1), Convert.ToInt32(br1));
+        }
+        public static Number operator -(Number a, Number b)
+        {
+            long ar1 = Convert.ToInt64(a.N);
+            long br1 = Convert.ToInt64(a.M);
+            long ar2 = Convert.ToInt64(b.N);
+            long br2 = Convert.ToInt64(b.M);
+            ar1 = ar1 * br2 - br1 * ar2;
+            br1 = br1 * br2;
+            ToCut(ref ar1, ref br1);
+            return new Number(Convert.ToInt32(ar1), Convert.ToInt32(br1));
+        }
+        public static Number operator *(Number a, Number b)
+        {
+            long ar1 = Convert.ToInt64(a.N);
+            long br1 = Convert.ToInt64(a.M);
+            long ar2 = Convert.ToInt64(b.N);
+            long br2 = Convert.ToInt64(b.M);
+            ar1 = ar1 * ar2;
+            br1 = br1 * br2;
+            ToCut(ref ar1, ref br1);
+            return new Number(Convert.ToInt32(ar1), Convert.ToInt32(br1));
+        }
+        public static Number operator /(Number a, Number b)
+        {
+            if (b == 0)
+            {
+                throw new Exception("division by 0 is impossible");
+            }
+            long ar1 = Convert.ToInt64(a.N);
+            long br1 = Convert.ToInt64(a.M);
+            long ar2 = Convert.ToInt64(b.N);
+            long br2 = Convert.ToInt64(b.M);
+            ar1 = ar1 * br2;
+            br1 = br1 * ar2;
+            ToCut(ref ar1, ref br1);
+            return new Number(Convert.ToInt32(ar1), Convert.ToInt32(br1));
+        }
+        public static Number operator ++(Number a)
+        {
+            return new Number(Convert.ToInt32(a.N+=a.M), Convert.ToInt32(a.M));
+        }
+        public static Number operator --(Number a)
+        {
+            return new Number(Convert.ToInt32(a.N -= a.M), Convert.ToInt32(a.M));
+        }
+        public static bool operator ==(Number a, Number b) => a.Equals((object)b);
+        public static bool operator !=(Number a, Number b) => !(a.Equals((object)b));
+        public static bool operator >(Number a, Number b) => ((IEquatable < Number > )a).IsBigger(b);
+        public static bool operator <(Number a, Number b) => ((IEquatable<Number>)b).IsBigger(a);
+        public static bool operator >=(Number a, Number b) => ((IEquatable<Number>)a).IsBigger(b) || a.Equals(b);
+        public static bool operator <=(Number a, Number b) => ((IEquatable<Number>)b).IsBigger(a) || b.Equals(a);
+
+        public string DoubleView()
+        {
+            return ToString();
+        }
+        public string FactionView()
+        {
+            return $"{N}/{M}";
+        }
+
+        public override string ToString()
+        {
+            return ToSingle().ToString();
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() % 0xAB;
+        }
+        public override bool Equals(object other)
+        {         
+            if (other == null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            if (GetType() != other.GetType())
+                return false;
+            return Equals(other as Number);
+        }
+
+        public bool Equals(Number other)
+        {
+            if (other == null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            if (GetType() != other.GetType())
+                return false;
+            if (M.Equals(other.M) && N.Equals(other.N))
+                return true;
+            else
+                return false;
+        }
+
+
+        bool IEquatable<Number>.IsBigger(Number a)
+        {
+            return ToSingle() > a.ToSingle() ? true : false;
+        }
+        private float ToSingle()
+        {
+            return N / Convert.ToSingle(M);
+        }
+        private static void ToCut(ref int a, ref int b)
+        {
+            int i = 2;
+            int max = a < b ? Math.Abs(a/2) : Math.Abs(b/2);
+            while (i <= max) 
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    a /= i;
+                    b /= i;
+                    i = 1;
+                }
+                i++;
+            }
+        }
+        private static void ToCut(ref long a, ref long b)
+        {
+            long i = 2;
+            long max = a < b ? Math.Abs(a / 2) : Math.Abs(b / 2);
+            while (i <= max)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    a /= i;
+                    b /= i;
+                    i = 1;
+                }
+                i++;
+            }
+        }
+        private void SetValues(int N, int M)
+        {
+            this.N = N;
+            this.M = M;
+        }
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 число:");
+            Number a = GetFloatFromUser();
+
+            Console.WriteLine("2 число:");
+            Console.WriteLine("N:");
+            int n = GetInt32FromUser(-10000000, 10000000);
+            Console.WriteLine("M:");
+            Number b = new Number(n,GetInt32FromUser(1, 1000000000));
+            Console.WriteLine();
+
+            Console.WriteLine($"1 число = {a.DoubleView()} = {a.FactionView()}");
+            Console.WriteLine($"2 число = {b.DoubleView()} = {b.FactionView()}");
+            Console.WriteLine();
+
+            Console.WriteLine($"1 число ++ = {a++}");
+            Console.WriteLine($"1 число -- = {a--}");
+            Console.WriteLine();
+
+            Console.WriteLine($"1 число == 2 числу: {a==b}");
+            Console.WriteLine($"1 число != 2 числу: {a!=b}");
+            Console.WriteLine();
+
+            Console.WriteLine($"1 число > 2 числа: {a > b}");
+            Console.WriteLine($"1 число < 2 числа: {a < b}");
+            Console.WriteLine($"1 число >= 2 числа: {a >= b}");
+            Console.WriteLine($"1 число <= 2 числа: {a <= b}");
+            Console.WriteLine();
+
+            Console.WriteLine($"1 число + 2 число: {a + b}");
+            Console.WriteLine($"1 число - 2 число: {a - b}");
+            Console.WriteLine();
+
+            Console.WriteLine($"1 число * 2 число: {a * b}");
+            if (b != 0)
+            {
+                Console.WriteLine($"1 число / 2 число: {a / b}");
+            }
+            else
+            {
+                Console.WriteLine("Division by 0 is impossible");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(((Number)53).FactionView());
+            Console.WriteLine(((Number)44.32f).FactionView());
+            try
+            {
+                Console.WriteLine(((Number)"32.4").FactionView());
+            }
+            catch
+            {
+                Console.WriteLine("NaN");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            Console.ReadKey();
+        }
+
+        static int GetInt32FromUser(int from, int to)
+        {
+            int number;
+            Console.WriteLine($"Введите целое число от {from} to {to}:");
+            while (!int.TryParse(Console.ReadLine(), out number) || number < from || number > to)    
+            {
+                Console.WriteLine("Ошибка ввода! Введите еще раз:");
+            }
+            return number;
+        }
+
+        static float GetFloatFromUser()
+        {
+            float number;
+            Console.WriteLine($"Введите вещественное число:");
+            while (true)
+            {
+                try
+                {
+                    number = Convert.ToSingle(Console.ReadLine());
+                    if (number < 10000000)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка ввода! Введите еще раз:");
+                }
+            }
+            return number;
+        }
+    }
+}
